cbe522472936c8be865bcc91b727ea6500e049bf
kshitijfulera <fulerakshitij2005@gmail.com>
2026-03-14 06:07:03 +0530

    First commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-red-800 text-3xl hover:text-blue-700 font-semibold">
+        AI Level Balancer
+      </h1>
+    </main>
+  )
+}
